91999fe251782a90eae60de7f17b4f3cc65f6e31
nineteenmg <kapyctarylit@mail.com>
2025-09-02 23:02:02 +0300

    Сообщение коммита

--- src/main/java/com/nineteenmg/bot/Main.java
+++ src/main/java/com/nineteenmg/bot/Main.java
@@ -5,13 +5,18 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Main started");
         try {
+            System.out.println("Creating TelegramBotsApi...");
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
-            MoneyBot bot = new MoneyBot();  // создаём один экземпляр
-            botsApi.registerBot(bot);       // регистрируем для Polling
+            System.out.println("Creating MoneyBot...");
+            MoneyBot bot = new MoneyBot();
+            System.out.println("Registering bot...");
+            botsApi.registerBot(bot);
             System.out.println("Бот запущен!");
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
 }
